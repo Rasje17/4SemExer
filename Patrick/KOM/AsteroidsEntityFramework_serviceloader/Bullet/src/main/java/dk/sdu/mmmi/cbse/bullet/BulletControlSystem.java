c9172642aa8dd77ce706707f71a684766ed4a853
pachr16 <32641694+pachr16@users.noreply.github.com>
2019-04-22 12:54:40 +0200

    mappestruktur

---
+++ Patrick/KOM/AsteroidsEntityFramework_serviceloader/Bullet/src/main/java/dk/sdu/mmmi/cbse/bullet/BulletControlSystem.java
@@ -0,0 +1,42 @@
+package dk.sdu.mmmi.cbse.bullet;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+
+public class BulletControlSystem implements IEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity bullet : world.getEntities(Bullet.class)) {
+            PositionPart positionPart = bullet.getPart(PositionPart.class);
+            MovingPart movingPart = bullet.getPart(MovingPart.class);
+            
+            movingPart.process(gameData, bullet);
+            positionPart.process(gameData, bullet);
+            
+            updateShape(bullet);
+        }
+    }
+    
+    private void updateShape(Entity entity) {
+        float[] shapex = entity.getShapeX();
+        float[] shapey = entity.getShapeY();
+        PositionPart positionPart = entity.getPart(PositionPart.class);
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+
+        shapex[0] = (float) (x + Math.cos(radians) * 3);
+        shapey[0] = (float) (y + Math.sin(radians) * 3);
+
+        shapex[1] = (float) (x + Math.cos(radians - 3.1415f) * 3);
+        shapey[1] = (float) (y + Math.sin(radians - 3.1145f) * 3);
+
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    }
+}
