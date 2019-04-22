ce50386409e49a9cd6200a6da88fc80610b3d393
Patrick <larsen.privat@gmail.com>
2019-04-23 00:52:46 +0200

    silentupdate kind of works

--- Patrick/KOM/Asteroids_NetbeansLab/SilentUpdate/src/main/java/sem4/silentupdate/UpdateActivator.java
+++ Patrick/KOM/Asteroids_NetbeansLab/SilentUpdate/src/main/java/sem4/silentupdate/UpdateActivator.java
@@ -1,24 +1,18 @@
 package sem4.silentupdate;
 
-/**
- *
- */
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import org.openide.modules.ModuleInstall;
 
-/**
- * Manages a module's lifecycle. Remember that an installer is optional and
- * often not needed at all.
- */
+
+
 public class UpdateActivator extends ModuleInstall {
 
     private final ScheduledExecutorService exector = Executors.newScheduledThreadPool(1);
 
     @Override
     public void restored() {
-        System.out.println("I WAS HERE!!*********");
         exector.scheduleAtFixedRate(doCheck, 5000, 5000, TimeUnit.MILLISECONDS);
     }
 
