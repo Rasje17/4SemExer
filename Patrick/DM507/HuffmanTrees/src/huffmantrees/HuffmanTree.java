22c5f61fa94297d103627e4737005b9758d6db3c
Rasmus Jensen <rasje17@student.sdu.dk>
2019-05-09 22:12:28 +0200

    test

--- Patrick/DM507/HuffmanTrees/src/huffmantrees/HuffmanTree.java
+++ Patrick/DM507/HuffmanTrees/src/huffmantrees/HuffmanTree.java
@@ -2,25 +2,35 @@ package huffmantrees;
 
 public class HuffmanTree {
     
-    private Element root;
-    private int leaves;
-    private String[] hCodes;
+    private Element root;   // the root element used in the tree
+    private int leaves; // the number of leves that will be in the tree
+    private String[] hCodes;    //the huffmancodes for each of the indexes
     
+    //Creating the tree bu reciving an array of occurences of each int index in the sorce material and generating a huffmantree from it and renerating the array of the huffman codes for each index
     public HuffmanTree(int[] occurences) {
         root = generateHuffmanTree(occurences);
         generateCodes();
     }
     
+    /**
+     * Method for generating the huffmantree, it usees a Minheap of Elements that contains a int key that reffers to the occurence from the arrau with the index saved in its value in a Node object.
+     * the 2 lowest element are then extracted from the heap and thir nods are sat as childs on a new node that is wraped in a Element which key is the combined sum of the kys of the children, then the new element is added back into the heap
+     * this continues until only one Element remains in the heap, this element is the root of the HuffmanTree.
+     * @param occurences the array of acurrences of each index in the surce
+     * @return returns the root element of the tree to store the tree and allow traversal of it
+     */
     private Element generateHuffmanTree(int[] occurences) {
         leaves = occurences.length;
-        PQHeap pq = new PQHeap(leaves);
+        PQHeap pq = new PQHeap(leaves); // a heap is used effectivly getting the element with the lowest key
         
+        // this creates notes of all elements in the occurence array and ads them to the minheap as the object part of Elements with thir occurenc as keys
         for (int i = 0; i < leaves; i++) {
             Node node = new Node(i);
             Element element = new Element(occurences[i], node);
             pq.insert(element);
         }
         
+        // Runs trhogh the heap and extracts the lower 2 elements to combine thir nodes into a new node and returns it as a new element
         for (int i = 0; i < leaves-1; i++) {
             Element ele1 = pq.extractMin();
             Element ele2 = pq.extractMin();
@@ -34,12 +44,21 @@ public class HuffmanTree {
         return pq.extractMin();
     }
     
+    /**
+     * Method that initiates the recursive traversal of the tree to generate the huffmancodes for each index
+     */
     private void generateCodes() {
         hCodes = new String[leaves];
         String code = "";
         traversal((Node) root.getData(), code, null);
     }
     
+    /**
+     * recursive method that builds huffmancodes as it traverses down the tree, when it reaches the end of a branch it writes the  gathered code as a String in an array at the index of the leaves value
+     * @param x the curent node
+     * @param code the sting af the so far path
+     * @param parent the parent node, used when reaching a nonexisting node to define its parent as a leave
+     */
     private void traversal(Node x, String code, Node parent) {
         if (x != null) {
             System.out.println(code);
@@ -55,6 +74,11 @@ public class HuffmanTree {
         return this.root;
     }
     
+    /**
+     * return the Huffman code for a given value
+     * @param index the value of a byte
+     * @return 
+     */
     public String encode(int index) {
         System.out.println("We're encoding! This is the result: " + hCodes[index]);
         return hCodes[index];
