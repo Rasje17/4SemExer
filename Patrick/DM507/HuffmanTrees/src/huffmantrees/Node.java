8e2dee2e93b421b4d597bd3c66ce7ba8a9758877
Patrick <larsen.privat@gmail.com>
2019-05-09 20:12:05 +0200

    Algoritmer del 3

---
+++ Patrick/DM507/HuffmanTrees/src/huffmantrees/Node.java
@@ -0,0 +1,42 @@
+package huffmantrees;
+
+/**
+ *
+ * @author Patrick Christoffersen - pachr16@student.sdu.dk
+ * @author Rasmus Jensen - rasje17@student.sdu.dk
+ */
+public class Node {
+    
+    private int value;
+    private Node leftChild = null, rightChild = null;
+    
+    public Node(int value) {
+        this.value = value;
+    }
+    
+    public Node(Node leftChild, Node rightChild) {
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+    
+    public Node getLeft(){
+        return this.leftChild;
+    }
+
+    public Node getRight(){
+        return this.rightChild;
+    }  
+    
+    public int getByte(){
+        return this.value;
+    }
+    
+    public void setLeft(Node newLeft){
+        this.leftChild = newLeft;
+    }
+    
+    public void setRight(Node newRight){
+        this.rightChild = newRight;
+    }
+    
+}
---
+++ Patrick/DM507/HuffmanTrees/src/huffmantrees/PQ.java
@@ -0,0 +1,6 @@
+package huffmantrees;
+
+public interface PQ {
+    public Element extractMin();
+    public void insert(Element e);
+}
