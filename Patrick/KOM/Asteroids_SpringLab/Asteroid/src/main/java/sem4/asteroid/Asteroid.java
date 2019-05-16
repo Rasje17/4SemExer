0c951ea7d72d4a00568efa2a0d88a7b425957f80
Patrick <larsen.privat@gmail.com>
2019-05-16 04:19:28 +0200

    SpringLab rename and last SilentUpdate

---
+++ Patrick/KOM/Asteroids_SpringLab/Asteroid/src/main/java/sem4/asteroid/Asteroid.java
@@ -0,0 +1,7 @@
+package sem4.asteroid;
+
+import sem4.common.data.Entity;
+
+public class Asteroid extends Entity {
+    
+}
---
+++ Patrick/KOM/Asteroids_SpringLab/Core/src/main/java/sem4/core/main/Game.java
@@ -0,0 +1,190 @@
+package sem4.core.main;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import sem4.common.data.Entity;
+import sem4.common.data.GameData;
+import sem4.common.data.World;
+import sem4.common.services.IEntityProcessingService;
+import sem4.common.services.IGamePluginService;
+import sem4.core.managers.GameInputProcessor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sem4.common.services.IPostEntityProcessingService;
+
+public class Game implements ApplicationListener {
+
+    private static OrthographicCamera cam;
+    private ShapeRenderer sr;
+    private final Lookup lookup = Lookup.getDefault();
+
+    private final GameData gameData = new GameData();
+//    private List<IEntityProcessingService> entityProcessors = new ArrayList<>();
+//    private List<IGamePluginService> entityPlugins = new ArrayList<>();
+    private World world = new World();
+    private List<IGamePluginService> gamePlugins = new CopyOnWriteArrayList<>();
+    private Lookup.Result<IGamePluginService> result;
+    private ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"enemyBeans.xml", "playerBeans.xml"});
+    
+    @Override
+    public void create() {
+
+        gameData.setDisplayWidth(Gdx.graphics.getWidth());
+        gameData.setDisplayHeight(Gdx.graphics.getHeight());
+
+        cam = new OrthographicCamera(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        cam.translate(gameData.getDisplayWidth() / 2, gameData.getDisplayHeight() / 2);
+        cam.update();
+
+        sr = new ShapeRenderer();
+
+        Gdx.input.setInputProcessor(new GameInputProcessor(gameData));
+
+//        IGamePluginService playerPlugin = new PlayerPlugin();
+//        IEntityProcessingService playerProcess = new PlayerControlSystem();
+//        entityPlugins.add(playerPlugin);
+//        entityProcessors.add(playerProcess);
+//        
+//        IGamePluginService enemyPlugin = new EnemyPlugin();
+//        IEntityProcessingService enemyProcess = new EnemyControlSystem();
+//        entityPlugins.add(enemyPlugin);
+//        entityProcessors.add(enemyProcess);
+//        
+//        IGamePluginService asteroidPlugin = new AsteroidPlugin();
+//        IEntityProcessingService asteroidProcess = new AsteroidControlSystem();
+//        entityPlugins.add(asteroidPlugin);
+//        entityProcessors.add(asteroidProcess);
+        
+
+        IGamePluginService playerPlugin = (IGamePluginService) context.getBean("playerPlugin", IGamePluginService.class);
+        IGamePluginService enemyPlugin = (IGamePluginService) context.getBean("enemyPlugin", IGamePluginService.class);
+        
+        playerPlugin.start(gameData, world);
+        gamePlugins.add(playerPlugin);
+        enemyPlugin.start(gameData, world);
+        gamePlugins.add(enemyPlugin);
+
+        result = lookup.lookupResult(IGamePluginService.class);
+        result.addLookupListener(lookupListener);
+        result.allItems();
+
+        for (IGamePluginService plugin : result.allInstances()) {
+            plugin.start(gameData, world);
+            gamePlugins.add(plugin);
+        }
+    }
+
+    @Override
+    public void render() {
+
+        // clear screen to black
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        gameData.setDelta(Gdx.graphics.getDeltaTime());
+
+        update();
+
+        draw();
+
+        gameData.getKeys().update();
+    }
+
+    private void update() {
+        
+        IEntityProcessingService enemyCS = (IEntityProcessingService) context.getBean("enemyControlSystem", IEntityProcessingService.class);
+        IEntityProcessingService playerCS = (IEntityProcessingService) context.getBean("playerControlSystem", IEntityProcessingService.class);
+        
+        enemyCS.process(gameData, world);
+        playerCS.process(gameData, world);
+        
+        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
+            entityProcessorService.process(gameData, world);
+        }
+        for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
+            postEntityProcessorService.process(gameData, world);
+        }
+        
+    }
+
+    private void draw() {
+        for (Entity entity : world.getEntities()) {
+
+            sr.setColor(1, 1, 1, 1);
+
+            sr.begin(ShapeRenderer.ShapeType.Line);
+
+            float[] shapex = entity.getShapeX();
+            float[] shapey = entity.getShapeY();
+
+            for (int i = 0, j = shapex.length - 1;
+                    i < shapex.length;
+                    j = i++) {
+
+                sr.line(shapex[i], shapey[i], shapex[j], shapey[j]);
+            }
+
+            sr.end();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+
+    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
+        return lookup.lookupAll(IEntityProcessingService.class);
+    }
+
+    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
+        return lookup.lookupAll(IPostEntityProcessingService.class);
+    }
+
+    private final LookupListener lookupListener = new LookupListener() {
+        @Override
+        public void resultChanged(LookupEvent le) {
+
+            Collection<? extends IGamePluginService> updated = result.allInstances();
+
+            for (IGamePluginService us : updated) {
+                // Newly installed modules
+                if (!gamePlugins.contains(us)) {
+                    us.start(gameData, world);
+                    gamePlugins.add(us);
+                }
+            }
+
+            // Stop and remove module
+            for (IGamePluginService gs : gamePlugins) {
+                if (!updated.contains(gs)) {
+                    gs.stop(gameData, world);
+                    gamePlugins.remove(gs);
+                }
+            }
+        }
+
+    };
+    
+}
